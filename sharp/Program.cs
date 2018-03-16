b437efa1d56bffb7634a47e97ef8ddb6518f2e7a
Drunut <Drunut@gmail.com>
2018-03-15 21:12:17 -0600

    Messing with Methods in C#

--- sharp/Program.cs
+++ sharp/Program.cs
@@ -2,11 +2,39 @@
 
 namespace sharp
 {
-    class Program
+    class Program // Class
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // primary run method, 'start method'
         {
-            Console.WriteLine("Hello World!");
+            // Step 2
+            Add(4, 8);
+            // Step 3
+            int result = Add(4, 8, 12);
+            if (result > 10)
+            {
+                Console.WriteLine("Result is larger then 10!");
+            }
+            else
+            {
+                Console.WriteLine("Result is smaller than or equal to 10");
+            }
+            // Step 4
+
+
+            Console.ReadKey();
         }
+
+        // Step 2 //
+        public static void Add(int num01, int num02)
+        {
+            Console.WriteLine(num01 + num02);
+        }
+        // Step 3 //
+        public static int Add(int num01, int num02, int num03)
+        {
+            return (num01 + num02 + num03);
+        }
+
+
     }
 }
